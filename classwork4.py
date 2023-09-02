5bde8c885bdad9504f8cf3567aaaae136588f075
ALHUSSAINAHMED <hussainalyasiri20@gmail.com>
2023-09-02 20:00:27 +0300

    classwork4

---
+++ classwork4.py
@@ -0,0 +1,44 @@
+def my_name() :
+    print("my name is hussain.")
+my_name()
+def my_meal(food,drink) :
+    print(f"I like to eat {food} and while drinking {drink}")
+my_meal("pizza","juice")
+
+def cube(number):
+    return number ** 3
+def by_three(number):
+    if number % 3==0:
+        return print(cube(number))
+    else:
+        return print(False)
+by_three(6)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
